8aca14ae0da606be1dfe8e2be36f0fdc40f46959
CastilhoBorges <148288141+CastilhoBorges@users.noreply.github.com>
2024-11-10 11:57:07 -0300

    Adicionando Logica para verificar se o email ou o usuario ja existe no banco antes de cadastrar

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,9 +1,31 @@
 import bcrypt from "bcrypt";
 import { createUserRepository } from "../respositories/user.repository.js";
+import { verifyUserEmailExist } from "../queries/verifyUserNameExist.js";
 
 export const createUserService = async (userData) => {
+  const { username, password, email } = userData;
+
+  const verifyEmailUsername = verifyUserEmailExist(username, email);
+
+  if (verifyEmailUsername) {
+    if (
+      verifyEmailUsername.email === email &&
+      verifyEmailUsername.username === username
+    ) {
+      throw new Error("Usuario ou Email ja existem!");
+    }
+
+    if (verifyEmailUsername.email === email) {
+      throw new Error("Este email ja existe!");
+    }
+
+    if (verifyEmailUsername.username === username) {
+      throw new Error("Este Username ja existe!");
+    }
+  }
+
   const saltRounds = 10;
-  const hashedPassword = await bcrypt.hash(userData.password, saltRounds);
-  const user = { ...userData, password: hashedPassword };
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  const user = { username: username, email: email, password: hashedPassword };
   return createUserRepository(user);
 };
