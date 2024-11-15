7518a4ac17f2b32c0cd2354aff992e60e5f3ae60
CastilhoBorges <148288141+CastilhoBorges@users.noreply.github.com>
2024-11-15 09:35:31 -0300

    Adicionando constante de Rotas no index

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,8 @@
-import { Router } from "express";
 import { helloWorld } from "./helloWorld.js";
 import { user } from "./user.routes.js";
+import { ROUTER } from "../utils/router.constants.js";
 
-const routes = Router();
+const routes = ROUTER;
 
 routes.use("/helloWorld", helloWorld);
 routes.use("/user", user);
